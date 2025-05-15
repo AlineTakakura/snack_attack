6686cdc9cc35b3d1cd73ff7caa743c565ec00d93
juliavfe <jvf@cesar.school>
2025-05-14 21:12:40 -0300

    add as funções

--- include/jogador.h
+++ include/jogador.h
@@ -1,6 +1,14 @@
 #ifndef JOGAGOR_H
 #define JOGADOR_H
 
+typedef struct {
+    int x,y;
+    char sprite;
+} Jogador;
+
+void initJogador(Jogador *j);
+void moverJogador(Jogador *j, char tecla);
+void desenharJogador(Jogador *j);
 
 
 #endif
